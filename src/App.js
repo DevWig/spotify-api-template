c04dea1d9e8eb7220452639ebea9dae4e3052017
Austin Wiginton <ahwig93@gmail.com>
2022-07-04 22:43:52 -0500

    Initial commit: Create React application and add needed files & folders to project directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import {SpotifyProvider} from './context/spotify/SpotifyContext';
+import Navbar from './components/layouts/Navbar';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SpotifyProvider>
+      <Router>
+        <div>
+          <div>
+            <Navbar />
+          </div>
+          <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          </main>
+        </div>
+      </Router>
+    </SpotifyProvider>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useContext} from 'react'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
