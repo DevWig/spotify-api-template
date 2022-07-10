ec38af8fbca38081665d4b47234681917fd4cd5d
Austin Wiginton <ahwig93@gmail.com>
2022-07-09 22:09:35 -0500

    Fix whitespace in App.js file for better formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
 import {SpotifyProvider} from './context/SpotifyContext';
 
 import './App.css'
 import Home from './pages/Home';
-
 
 function App() {
   return (
