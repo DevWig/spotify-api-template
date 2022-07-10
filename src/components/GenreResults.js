cf36aa173663bf177f318c39c8fa54bf749ab3f8
Austin Wiginton <ahwig93@gmail.com>
2022-07-09 22:12:07 -0500

    Complete setup of Home page and GenreResults components including all relevant styles

--- src/components/GenreResults.js
+++ src/components/GenreResults.js
@@ -1,8 +1,28 @@
 import React, {useContext} from 'react'
 
+import SpotifyContext from '../context/SpotifyContext'
+
 function GenreResults() {
+  const {genres, getPlaylists} = useContext(SpotifyContext)
+
   return (
-    <div>GenreResults</div>
+    <>
+      {genres.map((genre) => {
+        return (
+          <div className="card">
+            <img src={genre.icons[0].url} alt="Genre Cover" className="card-image"/>
+            <div className="card-body">
+              <h2 className="card-title">{genre.name}</h2>
+                <button className="link-btn" onClick={getPlaylists}>
+                  See Playlists
+                </button>
+            </div>
+          </div>
+        )
+            
+      })}
+    </>
+
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
 import React, {useEffect, useContext} from 'react'
 
+import './Home.css'
+import SpotifyContext from '../context/SpotifyContext'
+import GenreResults from '../components/GenreResults'
+
 function Home() {
+  const {getToken, getGenres} = useContext(SpotifyContext);
+
+  useEffect(() => {
+    getToken()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className="main">
+      <div className="nav-bar">
+        <button className="action-btn" onClick={getGenres}>
+          Get Genres
+        </button>
+      </div>
+      <div className="body">
+        <GenreResults />
+      </div>
+    </div>
   )
 }
 
